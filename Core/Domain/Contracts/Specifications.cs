b596b62378146373253237f1bfa9d39b216689b6
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-04-27 23:12:36 +0300

    Implemented Specification and Specification Evaluator for the brands and types

---
+++ Core/Domain/Contracts/Specifications.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public abstract class Specifications<T> where T:class
+    {
+        public Expression<Func<T, bool>>? Criteria { get; } // where
+
+        public List<Expression<Func<T, object>>> IncludeExpressions { get; } = new();
+
+        protected Specifications(Expression<Func<T, bool>>? creteria)
+        {
+            Criteria = creteria;
+        }
+
+        protected void AddInclude(Expression<Func<T, object>> expression)
+            => IncludeExpressions.Add(expression);
+
+
+    }
+}
+
+//where-> expression, func(T,bool)
+//include -> list of expression and returns object instead of bool
+
+
+
---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+    public class ProductWithBrandAndTypeSpecifications : Specifications<Product>
+    {
+        //use to retrieve product by id (where id, include brand/type)
+        // filter products by the id then include the brand and type
+        public ProductWithBrandAndTypeSpecifications(int id) : base(product => product.Id == id)
+        {
+
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+
+        }
+
+
+
+        // use to retrieve all products   (include brand/types)
+
+        public ProductWithBrandAndTypeSpecifications():base(null)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+
+    }
+}
