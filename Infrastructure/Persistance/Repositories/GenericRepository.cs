d525528a6cabfeca7aa73dcfc3fb981cc81da1d7
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-04-27 15:10:55 +0300

    Implemented Generic Repository and Unit of Work

---
+++ Infrastructure/Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Persistance.Data;
+
+namespace Persistance.Repositories
+{
+    public class GenericRepository<TEntity, Tkey> : IGenericRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public GenericRepository(AppDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+           await _dbContext.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking)=> asNoTracking?
+            await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync() //true
+            : await _dbContext.Set<TEntity>().ToListAsync();  //false
+
+
+
+        public async Task<TEntity?> GetByIdAsync(Tkey id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+
+    }
+}
---
+++ Infrastructure/Persistance/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Persistance.Data;
+
+namespace Persistance.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly ConcurrentDictionary<string, object> _repositories;
+
+        public UnitOfWork(AppDbContext dbContext) {
+            _dbContext = dbContext;
+            _repositories = new();
+        }
+        public IGenericRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>
+        {
+            //The commented method below is not good
+            // User can ask for many repsos in the same request
+            // return new GenericRepository<TEntity,Tkey>(_dbContext);
+
+            // Other Method: Use concurrent dictionaries
+            return (IGenericRepository<TEntity, Tkey>) _repositories.GetOrAdd(typeof(TEntity).Name, (_) => new GenericRepository<TEntity, Tkey>(_dbContext));
+
+        }
+      
+
+        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+    }
+}
