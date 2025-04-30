da6120252ec3ddace9c0919164287f60688733ba
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-04-30 06:20:22 +0300

    Implement IAuthenticationService in ServiceManager

--- Core/Services/ServiceManager.cs
+++ Core/Services/ServiceManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace Services
 {
@@ -10,14 +11,20 @@ namespace Services
     {
         private readonly Lazy<IProductService> _productService;
         private readonly Lazy<IBasketService> _basketService;
+        private readonly Lazy<IAuthenticationService> _authenicationService;
 
-        public ServiceManager(IUnitOfWork unitOfWork,IMapper mapper, IBasketRepository basketRepository) {
+
+
+        public ServiceManager(IUnitOfWork unitOfWork,IMapper mapper, IBasketRepository basketRepository, IAuthenticationService authenticationService, UserManager<User> userManager) {
             _productService=new Lazy<IProductService>(()=>new ProductService(unitOfWork,mapper));
             _basketService = new Lazy<IBasketService>(() => new BasketService(basketRepository, mapper));
+            _authenicationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager));
 
         }
         public IProductService ProductService => _productService.Value;
 
         public IBasketService BasketService => _basketService.Value;
+
+        public IAuthenticationService AuthenticationService => _authenicationService.Value;
     }
 }
