070ab765b909067b733f24a999e1e5c2483e286c
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-04-30 20:20:47 +0300

    Order Configurations

---
+++ Infrastructure/Persistance/Data/Configurations/OrderConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.OrderEntities;
+
+namespace Persistance.Data.Configurations
+{
+    internal class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(o => o.ShippingAddress, s => s.WithOwner());
+            builder.HasMany(o => o.OrderItems).WithOne();
+
+            // send to db as string and return from it as an enum
+            builder.Property(o => o.PaymentStatus).HasConversion(paymentStatus => paymentStatus.ToString(),
+                                                                  s=>Enum.Parse<OrderPaymentStatus>(s));
+
+            builder.HasOne(o => o.DeliveryMethod).WithMany().OnDelete(DeleteBehavior.SetNull)  ;
+            builder.Property(o => o.Subtotal).HasColumnType("decimal(18,3)");
+
+        }
+    }
+}
