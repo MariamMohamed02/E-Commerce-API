b735e6471d5218d6057354688ad164259467a110
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-05-04 07:07:43 +0300

    Created interface for order and all required Dto/s

---
+++ Core/Services.Abstraction/IOrderService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.OrderModels;
+
+namespace Services.Abstraction
+{
+    public interface IOrderService
+    {
+
+        //Get Order by Id  -> Takes: Guid id and returns OrderResult type
+        Task<OrderResult> GetOrderByIdAsync(Guid id);
+        //Get all orders for user by email -> email string and returns Ienumerable<OrderResult>
+        Task<IEnumerable<OrderResult>> GetAllOrdersByEmailAsync(string userEmail);
+        //Create order  -> takes: OrderRequest and email, returns OrderResult
+        Task<OrderResult> CreateOrderAsync(OrderResult request, string userEmail);
+        //Get all delivery Methods -> returns Ienumeearable<DeliveryMethodResult>
+        Task<IEnumerable<DeliveryMethodResult>> GetDeliveryMethodsAsync();
+
+
+    }
+}
