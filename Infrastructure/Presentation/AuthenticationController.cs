21b8eeba98d5d54ed5454b61571af72235d593f8
MariamMohamed02 <139442036+MariamMohamed02@users.noreply.github.com>
2025-05-08 14:57:53 +0300

    Implemented the previous functions endpoints in Authentication Controller

--- Infrastructure/Presentation/AuthenticationController.cs
+++ Infrastructure/Presentation/AuthenticationController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Abstraction;
 using Shared.Dtos;
+using Shared.OrderModels;
 
 namespace Presentation
 {
@@ -27,6 +30,41 @@ namespace Presentation
 
             return Ok(result);
         }
+
+        [HttpGet("EmailExist")]
+        public async Task<ActionResult<bool>> CheckEmailExist(string email)
+        {
+            return Ok(await serviceManager.AuthenticationService.CheckIfEmailExist(email));
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserResultDto>> GetCurrentUser()
+        {
+            var email =User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.AuthenticationService.GetUserByEmail(email);  
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("Address")]
+        public async Task<ActionResult<AddressDto>> GetAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.AuthenticationService.GetUserAddress(email);  
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpPut("Address")]
+        public async Task<ActionResult<AddressDto>> UpdateAddress(AddressDto address)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result= await serviceManager.AuthenticationService.UpdateUserAddress(address, email);
+            return Ok(result);
+
+        }
+
 
     }
 }
